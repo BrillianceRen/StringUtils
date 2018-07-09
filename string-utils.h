0e024a2b3961da6a19176775f36d268c96e9653c
BrillianceRen <ren0065@gmail.com>
2018-07-09 12:03:43 +0800

    add StringFormat
    from https://gist.github.com/Zitrax/a2e0040d301bf4b8ef8101c0b1e3f1d5

--- string-utils.h
+++ string-utils.h
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <memory>
 
 #ifndef STRING_UTILS_H
 #define STRING_UTILS_H
@@ -125,5 +127,35 @@ static inline void WriteFile(const std::string &filepath, const std::string &con
 }
 
 
+/**
+ * Convert all std::strings to const char* using constexpr if (C++17)
+ */
+template<typename T>
+static auto Convert(T&& t) {
+  if constexpr (std::is_same<std::remove_cv_t<std::remove_reference_t<T>>, std::string>::value) {
+    return std::forward<T>(t).c_str();
+  }
+  else {
+    return std::forward<T>(t);
+  }
+}
+
+/**
+ * printf like formatting for C++ with std::string
+ * Original source: https://stackoverflow.com/a/26221725/11722
+ */
+template<typename ... Args>
+static std::string StringFormatInternal(const std::string& format, Args&& ... args)
+{
+  size_t size = snprintf(nullptr, 0, format.c_str(), std::forward<Args>(args) ...) + 1;
+  std::unique_ptr<char[]> buf(new char[size]);
+  snprintf(buf.get(), size, format.c_str(), args ...);
+  return std::string(buf.get(), buf.get() + size - 1);
+}
+
+template<typename ... Args>
+static std::string StringFormat(std::string fmt, Args&& ... args) {
+  return StringFormatInternal(fmt, Convert(std::forward<Args>(args))...);
+}
 
 #endif
